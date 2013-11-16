c857a65aea6df70c8b33f3485fb48578a41e6fbb
Juan Carlos Arocha <juan@localhost.localdomain>
2013-11-16 15:37:19 +0100

    Some modifications

--- channels.cpp
+++ channels.cpp
@@ -1,6 +1,7 @@
 /*OPENCV LIBRARIES*/
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 /*STANDARD LIBRARIES*/
@@ -9,9 +10,24 @@
 using namespace cv;
 using namespace std;
 
-int main(int argc, const char *argv[])
-{
-    Mat image, gray_img, YCrCb_img;    // Matrices de imagenes (normal y escala de grises)
+
+/*Function to extract edges from an image as a signature*/
+Mat edges_signature(Mat image) {
+    Mat edges;
+    
+    return edges;
+}
+
+
+/*Function that generates signatures applying DCT transformation*/
+int* generate_signature(Mat image) {
+    return 0;
+}
+
+
+/*MAIN*/
+int main(int argc, const char *argv[]) {
+    Mat image, other_img, YCrCb_img;    // Matrices de imagenes (normal y escala de grises)
     vector<Mat> channels;   // Vector de matrices para los canales de RGB
     vector<Mat> mod(3);   // Vector de matrices para los canales de RGB
 
@@ -31,43 +47,10 @@ int main(int argc, const char *argv[])
         return -1;
     }
 
+    other_img = image.rowRange(100, 200).colRange(100,200);
 
-    /*Creacion de imagen con escala de grises*/
-    cvtColor( image, gray_img, CV_BGR2GRAY );
-
-
-
-    /*Creacion de ventana para mostrar imagenes*/
-    namedWindow("Display window", CV_WINDOW_AUTOSIZE);
-    
-    imshow("Display window", gray_img );
+    imshow("Display window", other_img );
     waitKey(0);
-
-    /*Separacion de canales*/
-    cv::split(image, channels);
-
-    imshow("Display window",  channels[0]);
-    waitKey(0);
-
-    imshow("Display window",  channels[1]);
-    waitKey(0);
-
-    imshow("Display window",  channels[2]);
-    waitKey(0);
-
-/*
-
-    mod[0] = channels[1];
-    mod[1] = channels[0];
-    mod[2] = channels[2];
-
-    cv::merge(mod, image);
-    imshow("Display window", image );
-    waitKey(0);
-
-*/
-
-
 
     /*Creacion de imagen a formato YCrCb*/
     cvtColor(image,YCrCb_img,CV_BGR2YCrCb);
@@ -88,6 +71,8 @@ int main(int argc, const char *argv[])
 
     imshow("Display window",  channels[2]);
     waitKey(0);
+
+    cout << YCrCb_img.size() << endl;
 
     
 }
